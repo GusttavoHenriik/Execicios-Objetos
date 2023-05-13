443cc5ff48f3648c8de54d720b25a23bf4467a0a
GusttavoHenriik <gusttavopfhenrique@gmail.com>
2023-05-13 17:49:49 -0300

    Concluido

---
+++ 01.Cadastro de usuário.js
@@ -0,0 +1,17 @@
+const endereco = {
+    rua: "Dez",
+    numero: 48,
+    complemento: "Casa",
+    bairro: "Jardim Primavera",
+    cep: "33.933-720",
+    cidade: "Ribeirão das Neves",
+    pais: "Brazil"
+}
+
+const usuario = {
+    nome: "Gustavo",
+    email: "gusttavopfhenrique@gmail.com",
+    telefone: "(31) 97205-9187",
+    data_nascimento: "28/09/2002",
+    endereco
+};
---
+++ 02.Lista de veículos.js
@@ -0,0 +1,25 @@
+const carros = [
+{
+    marca: "Fiat",
+    modelo: "Cronos",
+    ano: 2022,
+    cor: "Branco",
+    quantidade_portas: 4,
+    automatico: true
+},
+{
+    marca: "Volkswagem",
+    modelo: "Gol",
+    ano: 2022,
+    cor: "Preto",
+    quantidade_portas: 4,
+    automatico: false
+},
+{
+    marca: "Honda",
+    modelo: "Civic",
+    ano: 2022,
+    cor: "Azul",
+    quantidade_portas: 4,
+    automatico: true
+}];
